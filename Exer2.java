4f3204d24b168d82e459751c51bee2408b29f7a1
AlexVito96 <59684198+AlexVito96@users.noreply.github.com>
2020-01-27 17:27:22 -0800

    Add files via upload

---
+++ Exer2.java
@@ -0,0 +1,22 @@
+
+public class Exer2{
+
+    public static void main(final String args[]) {
+
+        // code
+
+        // variables DataType NameVariable = Value;
+
+    
+        //Declare Variables
+        String name = "Alex Vito";
+        int age = 23;
+        double salary = 1000000;
+
+        //Print out details
+        System.out.println(" My name is " + name + ", my age is " + age + " and I want to earn $" + salary + " per year.");
+
+
+
+    }
+}
